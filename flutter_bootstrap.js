e05b490786a9212bf952dd2fa67822f67ee0629b
gokulavi <gg8179871@gmail.com>
2026-06-28 13:01:42 +0000

    Deploying to gh-pages from @ gokulavi/open-cast@197fff2f4f334b64aa57dfb99d9bf37db2f8eb19 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3351219310" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2385795821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
